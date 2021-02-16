b258d8aca27d584a0a4c75e87a452a8d5fe86077
George Dinkov <george.dinkov@botronsoft.com>
2021-02-16 18:28:22 +0200

    Revert all commits up to first setup

--- app/extension/jira/extension_locust.py
+++ app/extension/jira/extension_locust.py
@@ -6,32 +6,23 @@ logger = init_logger(app_type='jira')
 
 @jira_measure("locust_app_specific_action")
 def app_specific_action(locust):
-    webSudoBody = {"webSudoPassword"="admin"}
-    r = locust.post('secure/admin/WebSudoAuthenticate.jspa', params=webSudoBody, catch_response=True)
- 
-    r = locust.get('/secure/ConfigurationIntegrityCheck.jspa#system', catch_response=True)  # call app-specific GET endpoint
-    # content = r.content.decode('utf-8')   # decode response content
+    r = locust.get('/app/get_endpoint', catch_response=True)  # call app-specific GET endpoint
+    content = r.content.decode('utf-8')   # decode response content
 
-    # token_pattern_example = '"token":"(.+?)"'
-    # id_pattern_example = '"id":"(.+?)"'
-    # token = re.findall(token_pattern_example, content)  # get TOKEN from response using regexp
-    # id = re.findall(id_pattern_example, content)    # get ID from response using regexp
+    token_pattern_example = '"token":"(.+?)"'
+    id_pattern_example = '"id":"(.+?)"'
+    token = re.findall(token_pattern_example, content)  # get TOKEN from response using regexp
+    id = re.findall(id_pattern_example, content)    # get ID from response using regexp
 
-    # logger.locust_info(f'token: {token}, id: {id}')  # log info for debug when verbose is true in jira.yml file
-    # if 'Configuration Integrity Check' not in content:
-    #     logger.error(f"'Configuration Integrity Check' was not found in {content}")
-    # assert 'Configuration Integrity Check' in content  # assert specific string in response content
+    logger.locust_info(f'token: {token}, id: {id}')  # log info for debug when verbose is true in jira.yml file
+    if 'assertion string' not in content:
+        logger.error(f"'assertion string' was not found in {content}")
+    assert 'assertion string' in content  # assert specific string in response content
 
-    # body = {"id": id, "token": token}  # include parsed variables to POST request body
-    checkBody = {"name":"","projectKeys":"","description":"","type":"system","isScoped":"false","includeAllFilters":"false","includeAllBoards":"false","includeAllDashboards":"false","includeProjectFilters":"false","includeProjectBoards":"false"}
-    apiCheckBody = {"scope" : "system"}
+    body = {"id": id, "token": token}  # include parsed variables to POST request body
     headers = {'content-type': 'application/json'}
-    r = locust.post('/rest/integrity-check/1.0/integrity', json=checkBody, headers=headers, catch_response=True)  # call app-specific POST endpoint
-
-    hLocation = r.headers['Location'].split("aws.com")[1]
-
-    r = locust.get(hLocation, catch_response=True)
-    # content = r.content.decode('utf-8')
-    # if 'Integrity Check' not in content:
-    #     logger.error(f"'Integrity Check ' was not found in {content}")
-    # assert 'Integrity Check ' in content  # assertion after POST request
+    r = locust.post('/app/post_endpoint', body, headers, catch_response=True)  # call app-specific POST endpoint
+    content = r.content.decode('utf-8')
+    if 'assertion string after successful POST request' not in content:
+        logger.error(f"'assertion string after successful POST request' was not found in {content}")
+    assert 'assertion string after successful POST request' in content  # assertion after POST request
